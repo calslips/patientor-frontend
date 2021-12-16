f4cf8ab206098098d4af237ce2038bed48c1a2a6
Caleb Phillips <csphillips5@gmail.com>
2021-12-15 22:06:17 -0500

    9.20 extend patient page to list each entry date, description & diagnosis codes

--- src/SinglePatientPage/index.tsx
+++ src/SinglePatientPage/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { Icon } from 'semantic-ui-react';
+import { Icon, List } from 'semantic-ui-react';
 
 import { Patient } from '../types';
 import { apiBaseUrl } from '../constants';
@@ -24,37 +24,37 @@ const SinglePatientPage = () => {
 
   if (!currentPatient || !(currentPatient.id === id)) {
     void getSinglePatientData();
-
-    return (
-      <div>
-        <h2>
-          {currentPatient?.name} {currentPatient?.gender === 'female'
-            ? <Icon name='venus' />
-            : currentPatient?.gender === 'male'
-              ? <Icon name='mars' />
-              : <Icon name='genderless' />
-          }
-        </h2>
-        <p>ssn: {currentPatient?.ssn}</p>
-        <p>occupation: {currentPatient?.occupation}</p>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h2>
-          {currentPatient?.name} {currentPatient?.gender === 'female'
-            ? <Icon name='venus' />
-            : currentPatient?.gender === 'male'
-              ? <Icon name='mars' />
-              : <Icon name='genderless' />
-          }
-        </h2>
-        <p>ssn: {currentPatient.ssn}</p>
-        <p>occupation: {currentPatient.occupation}</p>
-      </div>
-    );
   }
+
+  return (
+    <div>
+      <h2>
+        {currentPatient?.name} {currentPatient?.gender === 'female'
+          ? <Icon name='venus' />
+          : currentPatient?.gender === 'male'
+            ? <Icon name='mars' />
+            : <Icon name='genderless' />
+        }
+      </h2>
+      <p>ssn: {currentPatient?.ssn}</p>
+      <p>occupation: {currentPatient?.occupation}</p>
+      <h3>entries</h3>
+      {currentPatient?.entries.map(entry => (
+        <div key={entry.id}>
+          <List>
+            <List.Item>{entry.date} <em>{entry.description}</em>
+            <List bulleted>
+              {entry.diagnosisCodes?.map(code => (
+                <List.Item key={code}>{code}</List.Item>
+              ))}
+            </List>
+            </List.Item>
+          </List>
+        </div>
+      ))}
+    </div>
+  );
+
 };
 
 export default SinglePatientPage;
